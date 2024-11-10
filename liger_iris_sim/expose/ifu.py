units should be correct now

liger_iris_sim/expose/ifu.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 
 def expose_ifu(
         source_cube : np.ndarray,
+        wavebins : np.ndarray,
         itime : float, n_frames : int,
         collarea : float,
         sky_background : np.ndarray,
@@ -19,39 +20,40 @@ def expose_ifu(
     ) -> dict:
     """
     Parameters:
-    source_cube (np.ndarray): Source cube (y, x, wave). Each wave bin in units of photons / sec / m^2 / micron.
+    source_cube (np.ndarray): Source cube (y, x, wave). Units are photons / sec / m^2 / wavebin.
+    wavebins (np.ndarray): Wavelength grid (microns).
     itime (float): Integration time (sec).
     collarea (np.ndarray): Collimating area (m^2)
-    sky_background (np.ndarray): The sky background spectrum sampled on the same
-        wave bins as source_cube. Each bin in sky_background in units of photons / sec / m^2 / micron.
+    sky_background (np.ndarray): The sky background spectrum sampled on wavebins
+        in units of photons / sec / m^2 / wavebin.
     efftot (np.ndarray): Total efficiciency (convert photons -> e-)
     gain (float): Detector gain (e-)
     
     Returns:
     dict: The exposure components. Keys are:
-        observed (np.ndarray): Combined exposure (e- / s / micron)
-        source (np.ndarray): Source signal (e- / s / micron)
-        background (np.ndarray): Background signal (e- / s / mircon)
+        observed (np.ndarray): Combined exposure (e- / s / wavebin)
+        source (np.ndarray): Source signal (e- / s / wavebin)
+        background (np.ndarray): Background signal (e- / s / wavebin)
         snr (np.ndarray): SNR
-        noise (np.ndarray): Noise (e- / s / micron)
-        read_noise (np.ndarray): Effective read noise (e- / s)
+        noise (np.ndarray): Noise (e- / s / wavebin)
+        read_noise (np.ndarray): Effective read noise (e- RMS wavebin)
     """
 
-    # Integrate over telescope aperture (photons / sec / micron)
+    # Integrate over telescope aperture (photons / sec / wavebin)
     source_rate = source_cube * collarea
     sky_background_rate = sky_background * collarea
 
-    # Efficiency (effectively converts to e- / s / micron)
+    # Efficiency (effectively converts to e- / s / wavebin)
     source_rate *= efftot
     sky_background_rate *= efftot
 
-    # Dark rate (e- / s / micron?)
+    # Dark rate (e- / s)
     dark_rate = dark_current * gain
 
-    # Total background rate (e- / s / micron)
+    # Total background rate (e- / s / wavebin)
     background_rate = sky_background_rate + dark_rate
 
-    # Integrate source and background over itime and frames (e- / micron)
+    # Integrate source and background over itime and frames (e- / wavebin)
     source_tot = source_rate * itime * n_frames
     background_tot = background_rate * itime * n_frames
 
@@ -61,14 +63,20 @@ def expose_ifu(
     # Final simulated image over all frames (e-)
     sim_tot = source_tot + background_tot
 
+    # Simulated noise
+    noise_tot = np.sqrt(sim_tot + (read_noise * np.sqrt(n_frames))**2)
+
     # Add poisson noise to final image (e-)
-    sim_tot_noisy = np.random.poisson(lam=sim_tot, size=source_cube.shape)
+    sim_tot_noisy = np.random.poisson(lam=sim_tot, size=source_cube.shape).astype(float)
+    #source_tot_noisy = np.random.poisson(lam=source_tot, size=source_cube.shape).astype(float)
+    #background_tot_noisy = np.random.poisson(lam=background_tot, size=background_tot.shape).astype(float)
+    #sim_tot_noisy = source_tot_noisy + background_tot_noisy
+    #sim_tot_noisy = sim_tot.copy()
+
+    #sim_tot_noisy = sim_tot + np.random.normal(loc=0, scale=noise_tot)
 
     # Add read noise to final image (broadcast to each wavelength slice)
     sim_tot_noisy = sim_tot_noisy + read_noise_tot[:, :, None]
-
-    # Simulated noise
-    noise_tot = np.sqrt(sim_tot + (read_noise * np.sqrt(n_frames))**2)
 
     # SNR
     snr = source_tot / noise_tot
@@ -76,13 +84,18 @@ def expose_ifu(
     # Convert back to e-/s
     sim_tot_noisy /= (n_frames * itime)
     source_tot /= (n_frames * itime)
+    #source_tot_noisy /= (n_frames * itime)
     background_tot /= (n_frames * itime)
+    #background_tot_noisy /= (n_frames * itime)
     noise_tot /= (n_frames * itime)
 
     # Outputs
     out = dict(
         observed=sim_tot_noisy,
-        source=source_tot, background=background_tot,
+        source=source_tot,
+        #source_observed=source_tot_noisy,
+        background=background_tot,
+        #background_observed=background_tot_noisy,
         snr=snr, noise=noise_tot,
         read_noise=read_noise_tot,
     )

liger_iris_sim/sky/background_sky.py
@@ -15,92 +15,55 @@ def get_maunakea_spectral_sky_background(
         gemini_file : str | None = None,
         ohlines_file : str | None = None,
         T_tel : float = 275, T_atm : float = 258, T_aos : float = 243, T_zod : float = 5800,
-        backmag : float = 22.0, imagmag : float = 20.0, zp : float = 25.0, zpphot : float = 25.0,
         Em_tel : float = 0.09, Em_atm : float = 0.2, Em_aos : float = 0.01, Em_zod : float = 1.47E-12,
     ):
     """
     Parameters:
-    wavelengths (np.ndarray): blah.
+    wavelengths (np.ndarray): The wavelength array.
     resolution (float): The spectral resolution (taken to be constant across bandpass here).
-    """
+    """ 
 
-    # Generate arrays
-    bbtel = np.zeros(wavelengths.size)  # telescope blackbody spectrum
-    bbaos = np.zeros(wavelengths.size)  # AO blackbody spectrum
-    bbatm = np.zeros(wavelengths.size)  # Atm blackbody spectrum
-    bbzod = np.zeros(wavelengths.size)  # Zodiacal light blackbody spectrum
-    bbspec = np.zeros(wavelengths.size)  # Total blackbody spectrum
+    sterr = 1 / 206265**2 # rad^2 / arcsec^2
+    dw = np.nanmedian(np.diff(wavelengths))
 
-    sterr2 = (1 / 206265**2)
+    # BB components
+    wavem = wavelengths * 1E-6
+    s1 = 2 * h * c**2 / wavem**5
+    s2 = h * c / (wavem * k)
+    Ephot = h * c / wavem # J / photon
+    # flux density: J / (s * m * m^2 * rad^2)
+    bbtel = s1 / (np.exp(s2 / T_tel) - 1) # telescope blackbody spectrum
+    bbaos = s1 / (np.exp(s2 / T_aos) - 1) # AO blackbody spectrum
+    bbatm = s1 / (np.exp(s2 / T_atm) - 1) # Atm blackbody spectrum
+    bbzod = s1 / (np.exp(s2 / T_zod) - 1) # Zodiacal light blackbody spectrum
+    bbtel /= Ephot # photons / (s * m * m^2 * rad^2)
+    bbaos /= Ephot
+    bbatm /= Ephot
+    bbzod /= Ephot
+    bbtel /= 1E6 # photons / (s * micron * m^2 * rad^2)
+    bbaos /= 1E6
+    bbatm /= 1E6
+    bbzod /= 1E6
+    bbtel *= sterr # photons / (s * micron * m^2 * arcsec^2)
+    bbaos *= sterr
+    bbatm *= sterr
+    bbzod *= sterr
+    bbtel *= dw # photons / (s * m^2 * arcsec^2 * wavebin)
+    bbaos *= dw
+    bbatm *= dw
+    bbzod *= dw
+    bbspec = bbtel * Em_tel \
+                + bbaos * Em_aos \
+                + bbatm * Em_atm
 
-    # Loop over the first and last pixel of the complete spectrum
-    for i in range(wavelengths.size):
-
-        # Wavelength in microns and meters
-        wave = wavelengths[i]
-        wavem = wave * 1E-6
-      
-        # Thermal Blackbodies in units J / (s * m * m^2 * rad^2)
-        s1 = 2 * h * c**2 / wavem**5
-        s2 = h * c / (wavem * k)
-        bbtel[i] = s1 / (np.exp(s2 / T_tel) - 1)
-        bbaos[i] = s1 / (np.exp(s2 / T_aos) - 1)
-        bbatm[i] = s1 / (np.exp(s2 / T_atm) - 1)
-        bbzod[i] = s1 / (np.exp(s2 / T_zod) - 1)
-        
-        # Convert to photons / (s * micron * m^2 * rad^2) for Vega conversion below
-        # Ephot = hc/lambda
-        Ephot = h * c / wavem # J / photon
-        bbtel[i] /= Ephot # photons / (s * m * m^2 * rad^2)
-        bbaos[i] /= Ephot
-        bbatm[i] /= Ephot
-        bbzod[i] /= Ephot
-        bbtel[i] /= 1E6 # photons / (s * micron * m^2 * rad^2)
-        bbaos[i] /= 1E6
-        bbatm[i] /= 1E6
-        bbzod[i] /= 1E6
-        bbtel[i] *= sterr2 # photons / (s * micron * m^2 * arcsec^2)
-        bbaos[i] *= sterr2
-        bbatm[i] *= sterr2
-        bbzod[i] *= sterr2
-
-        # Total BB together with emissivities from each component
-        # in units photons / (s * m^2 * micron * arcsecond)
-        # Only use the BB for the AO system and the telescope since 
-        # the Gemini observations already includes the atmosphere
-        bbspec[i] = bbtel[i] * Em_tel \
-                  + bbaos[i] * Em_aos
-
-    # OH lines
+    # OH lines: photons / (s * m^2 * arcsec^2 * wavebin)
     if ohsim:
         ohspec = sim_ohlines(wavelengths, ohlines_file=ohlines_file, resolution=resolution)
     else:
-
-        # Load Gemini file
-        with fits.open(gemini_file) as f:
-            gemini_spec = f[0].data
-            header = f[0].header
-
-        # Wave grid for Gemini spectrum
-        cdelt1 = header["CDELT1"] * 1E-4
-        crval1 = header["CRVAL1"] * 1E-4
-        nx = gemini_spec.shape[0]
-        gemini_wave = np.arange(nx) * cdelt1 + crval1
-
-        # Convolve 
-        delt = 2 * (wavelengths[1] - wavelengths[0]) / (gemini_wave[1] - gemini_wave[0])
-        if delt > 1:
-            stddev = delt / 2 * np.sqrt(2 * np.log(2))
-            x = np.arange(4 * int(delt) + 1) - 2 * int(delt)
-            psf = Gaussian1D(amplitude=1, stddev=stddev)(x)
-            psf /= psf.sum()
-            gemini_spec = np.convolve(gemini_spec, psf, mode='same')
-        
-        # Interpolate onto our wavelengths grid
-        ohspec = np.interp(wavelengths, gemini_wave, gemini_spec)
-
+        ohspec = get_gemini_background(gemini_file, wavelengths)
 
     # Combined background
+    # photons / (s * m^2 * arcsec^2 * wavebin)
     background = bbspec + ohspec
     
     # Results
@@ -111,6 +74,33 @@ def get_maunakea_spectral_sky_background(
     )
 
     return out
+
+def get_gemini_background(filename : str, wavelengths : np.ndarray):
+    # Load Gemini file
+    with fits.open(filename) as f:
+        gemini_spec = f[0].data
+        header = f[0].header
+
+    # Wave grid for Gemini spectrum
+    cdelt1 = header["CDELT1"] * 1E-4
+    crval1 = header["CRVAL1"] * 1E-4
+    nx = gemini_spec.shape[0]
+    gemini_wave = np.arange(nx) * cdelt1 + crval1
+
+    # Convolve
+    delt = 2 * (wavelengths[1] - wavelengths[0]) / (gemini_wave[1] - gemini_wave[0])
+    if delt > 1:
+        stddev = delt / 2 * np.sqrt(2 * np.log(2))
+        x = np.arange(4 * int(delt) + 1) - 2 * int(delt)
+        psf = Gaussian1D(amplitude=1, stddev=stddev)(x)
+        psf /= psf.sum()
+        gemini_spec = np.convolve(gemini_spec, psf, mode='same')
+    
+    # Interpolate onto our wavelengths grid
+    ohspec = np.interp(wavelengths, gemini_wave, gemini_spec)
+    
+    # Return
+    return ohspec
 
 
 def sim_ohlines(
@@ -149,9 +139,9 @@ def sim_ohlines_lorenztian(
     ):
     # flux units: photons / (m^2 * s * arcsec^2)
     fwhm = wavecenter / resolution
-    gamma = fwhm / 2
-    dl = np.median(np.diff(wavelengths))
     spec = Lorentz1D(amplitude=1, x_0=wavecenter, fwhm=fwhm)(wavelengths)
-    spec /= gamma * np.pi
-    spec *= dl * flux
+    tot = np.sum(spec)
+    # Out units = photons / (m^2 * s * arcsec^2 * wavebin)
+    spec /= tot
+    spec *= flux
     return spec

liger_iris_sim/utils/spec_utils.py
@@ -6,58 +6,15 @@ from specutils import Spectrum1D
 from specutils.manipulation import FluxConservingResampler
 import astropy.units as units
 
-def resample_spectrum(
-        wave : np.ndarray, spectrum : np.ndarray, wave_out : np.ndarray | None = None,
-        phot_flambda : float | None = None, phot_fint : float | None = None,
-        resolution : float | None = None
-    ):
-    """
-    Parameters:
-    wavelengths (np.ndarray): The wavelength grid in microns.
-    spectrum (np.ndarray): The spectrum in arbitrary units.
-    wave_out (np.ndarray): The output wavelength grid to sample on.
-    phot_flambda (float | None): The photon flux density in photons / sec / micron / m^2.
-    phot_fint (float | None): The integrated photon flux in photons / sec / m^2.
-    
-    Returns:
-    wavelengths_out (np.ndarray): The resampled wavelength grid in microns.
-    spectrum_out (np.ndarray): The spectrum sampled on wavelengths_out in photons / sec / microns / m^2.
-    """
 
-    # Output wavelength grid
-    if wave_out is None:
-        wave_out = wave.copy()
-        spectrum_out = spectrum.copy()
-    else:
-        # spectrum_out = FluxConservingResampler()(
-        #     Spectrum1D(
-        #         flux=spectrum * units.ph / (units.s * units.micron * units.m**2),
-        #         spectral_axis=wavelengths * units.micron
-        #     ),
-        #     wave_out * units.micron
-        # ).data
-        spectrum_out = np.interp(wave_out, wave, spectrum, left=0, right=0)
-        bad = np.where(~np.isfinite(spectrum_out) | (spectrum_out < 0))
-        spectrum_out[bad] = 0
-
-    # Convolve to resolution
-    if resolution is not None:
-        spectrum_out = convolve_spectrum(wave_out, spectrum_out, resolution=resolution)
-
-    # Normalize
-    spectrum_out /= np.sum(spectrum_out)
-
-    # Scale the spectrum
-    dl = np.median(np.diff(wave_out)) # spacing of wave grid in nm
-    if phot_flambda is not None:
-        spectrum_out *= phot_flambda * (wave_out[-1] - wave_out[0]) / np.sum(spectrum_out * dl)
-    elif phot_fint is not None:
-        spectrum_out *= phot_fint / np.sum(spectrum_out * dl)
-    else:
-        raise ValueError("phot_flambda and phot_fint are both None")
-
-    # Return
-    return wave_out, spectrum_out
+# #@njit
+# def bin_spectrum(wave, spec, wave_out):
+#     spec_out = np.zeros(wave_out.size)
+#     for i in range(spec_out.size):
+#         #ii = np.searchsorted(wave, wave_out[i], side='left')
+#         #jj = np.searchsorted(wave, wave_out[i]+1, side='left')
+#         #spec_out[i] = val
+#     return spec_out
 
 
 def convolve_spectrum(