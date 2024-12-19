d836e0e46ffd2d2ff789dec5bd8fcd8d74c0e0d1
astrobc1 <bryson.cale1@gmail.com>
2024-12-18 18:02:56 -0800

    filename functions only generate filenames not full paths

--- liger_iris_sim/iris/data.py
+++ liger_iris_sim/iris/data.py
@@ -2,7 +2,6 @@ def generate_filename(
         obsid : str ,
         detector : str, obstype : str, level : int | str = 0,
         exp : int | str = '0001', subarray : int | str | None = None,
-        dir : str = '',
     ):
     # Format: OBSID-Liger-DET[X]-OBSTYPLevel-EXP-SUB.fits
     if isinstance(exp, int):
@@ -11,4 +10,4 @@ def generate_filename(
         subarray = '-' + str(subarray).zfill(2)
     else:
         subarray = ''
-    return f"{dir}{obsid}_IRIS_{detector.upper()}_{obstype}_LVL{int(level)}_{exp}{subarray}.fits"
+    return f"{obsid}_IRIS_{detector.upper()}_{obstype}_LVL{int(level)}_{exp}{subarray}.fits"
